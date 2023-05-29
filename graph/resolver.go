582d5ee3d86c8d721fb8ec2aa304d190744694e6
Masuda-1246 <yaojiezengtian38@gmail.com>
2023-05-29 12:41:26 +0900

    grqphql moc data

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,11 @@
 package graph
 
+import "github.com/Masuda-1246/docker-go-graphql-postgre/graph/model"
+//go:generate go run github.com/99designs/gqlgen generate
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct{
+	todos []*model.Todo
+}
